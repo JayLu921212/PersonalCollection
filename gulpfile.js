5c625e5c40aa09edb532a7bddeba9d107718a1ac
陆俊杰 <517642043@qq.com>
2017-01-30 00:02:54 +0800

    no message

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+//导入工具包 require('node_modules里对应模块')
+var gulp = require('gulp'), //本地安装gulp所用到的地方
+    less = require('gulp-less');
+    connect = require('gulp-connect');
+ 
+//定义一个testLess任务（自定义任务名称）
+gulp.task('testLess', function () {
+    gulp.src('src/less/*.less') //该任务针对的文件
+        .pipe(less()) //该任务调用的模块
+        .pipe(gulp.dest('src/css'))//将会在src/css下生成index.css
+        .pipe(connect.reload()); 
+});
+//用gulp起web服务器
+gulp.task("buildweb",function () {
+	connect.server({
+		livereload: true
+	});
+});
+gulp.task('watch', function() {
+    gulp.watch('src/less/*.less', ['testLess']);
+});
+
+
+ 
+gulp.task('default',['testLess','buildweb','watch']); //定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
+ 
+//gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
+//gulp.src(globs[, options]) 执行任务处理的文件  globs：处理的文件路径(字符串或者字符串数组) 
+//gulp.dest(path[, options]) 处理完后文件生成路径
