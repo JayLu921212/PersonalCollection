4122222f06048ac9e5d73f4473b6c9aa03ef5ec3
jay <jay.lu@pwc.com>
2017-02-28 17:41:27 +0800

    20170228-save

--- src/log/Log20170227.js
+++ src/log/Log20170227.js
@@ -2,3 +2,6 @@ $(selector).animate(styles, speed, easing, callback)
 style 值只支持数字 ，//red不支持
 speed 'slow','fast','normal',number
 easing swing , linear ,其他需要插件
+
+
+http://www.codeceo.com/article/rendering-engine-and-front-end-optimization.html
