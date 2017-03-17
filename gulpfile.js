7ba3b8123c00bf9a0f5515f74d0c7d32ff121a02
jay <jay.lu@pwc.com>
2017-03-17 18:48:08 +0800

    20170317-save

--- gulpfile.js
+++ gulpfile.js
@@ -5,43 +5,54 @@ var gulp = require('gulp'), //本地安装gulp所用到的地方
     browserSync = require('browser-sync'),
     connect = require('gulp-connect'),
     inject = require('gulp-inject');
+    uglify  = require('gulp-uglify');
+    concat = require('gulp-concat');
+    minifyCss =  require('gulp-minify-css');
 
-//定义一个testLess任务（自定义任务名称）
-//这里.tmp文件夹下面必须再套一层src，不然样式读取不到
-// gulp.task('testLess', function () {
-//     gulp.src('src/less/*.less') //该任务针对的文件
-//         .pipe(less()) //该任务调用的模块
-//         .pipe(gulp.dest('.tmp/src/css'))//将会在src/css下生成index.css
-// });
-gulp.task('testLess', function() {
+gulp.task('compileLess', function () {
     gulp.src('src/less/*.less') //该任务针对的文件
         .pipe(less()) //该任务调用的模块
-        .pipe(gulp.dest('src/css')) //将会在src/css下生成index.css
+        .pipe(minifyCss())
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('./dist/src/css')) //将会在src/css下生成index.css
 });
-// browser 需要在制定目录下创建index.html
-gulp.task('serve', ['testLess'], function() {
-    browserSync.init({
-        //指定服务器启动根目录  
-        server: "./src"
-    });
+gulp.task('compressJs',function () {
+    gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('src/less/*.less', ['testLess','inject']);
-    //监听任何文件变化，实时刷新页面  
+gulp.task('watch', function () {
+    gulp.watch('src/less/*.less', ['compileLess', 'inject']);
+    //监听任何文件变化，实时刷新页面
     gulp.watch("./src/**/*.*").on('change', browserSync.reload);
 });
 
-gulp.task('inject', function() {
-    var target = gulp.src('./src/pages/test.html');
-    var source = gulp.src(['./src/js/*.js','./src/css/*.css'], {read: false});
+// browser 需要在制定目录下创建index.html
+gulp.task('serve', ['testLess'], function () {
+    browserSync.init({
+        //指定服务器启动根目录  
+        // server: "./src"
+        //http://www.browsersync.cn/docs/options/ 选项配置说明
+        server: {
+            baseDir: "src",
+            //注意这里不要用./
+            index: "index.html"
+        }
+    });
+});
+
+
+gulp.task('inject', function () {
+    var target = gulp.src('./dist/src/pages/*.html');
+    var source = gulp.src(['./dist/src/js/*.js', './dist/src/css/*.css'], {read: false});
     target.pipe(inject(source))
-          .pipe(gulp.dest('./src/pages'));
-})
+        .pipe(gulp.dest('./dist/src/pages'));
+});
 
 
-
-gulp.task('default', ['testLess', 'serve', 'watch','inject']); //定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
+gulp.task('default', ['compileLess', 'watch', 'inject', 'serve']); //定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
 
 //gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
 //gulp.src(globs[, options]) 执行任务处理的文件  globs：处理的文件路径(字符串或者字符串数组) 
---
+++ src/log/Log20170308.js
@@ -0,0 +1,8 @@
+var q = 'Bower install packages EPERM rename error';
+
+var a = "It's a bug in bower 1.2.7 and up related to an upgraded zip library. A workaround is reinstall bower, as it will force an upgrade for the zip library
+bower cache clean
+npm uninstall -g bower
+npm install -g bower";
+
+var r = "http://stackoverflow.com/questions/21337044/bower-install-packages-eperm-rename-error?answertab=active#tab-top";
